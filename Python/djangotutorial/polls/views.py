2c8fee0c922670f2539d319b4aec13e9bc845716
Mateusz <m.dyminczuk1@gmail.com>
2026-03-07 16:31:59 +0100

    ukonczone do lekcji 3

--- Python/djangotutorial/polls/views.py
+++ Python/djangotutorial/polls/views.py
@@ -1,4 +1,19 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("To glowna strona apk pools")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    return HttpResponse(f"Patrzysz na wyniki pytania o numerze {question_id}")
+
+def vote(request,question_id):
+    return HttpResponse(f"Głosujesz na pytanie o numerze {question_id}")
