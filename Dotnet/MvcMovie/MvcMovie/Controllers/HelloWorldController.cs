3b5bd71e12894daf6c73511d55dfa301b9fbae54
Mateusz <m.dyminczuk1@gmail.com>
2026-03-27 18:13:18 +0100

    Konfiguracja srodowiska i pierwsza strona HelloWorld

---
+++ Dotnet/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "To jest moja domyślna akcja...";
+    }
+
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "To jest metoda akcji Welcome...";
+    }
+}
