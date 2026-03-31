a54efb6cdabf0bbf5b194625bd3617b0de1d5ab7
Mateusz <m.dyminczuk1@gmail.com>
2026-03-31 18:33:01 +0200

    no message

--- Dotnet/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ Dotnet/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // GET: /HelloWorld/
-    public string Index()
+ 
+    public IActionResult Index()
     {
-        return "To jest moja domyślna akcja...";
+        return View();
     }
 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return "To jest metoda akcji Welcome...";
+        ViewData["Message"] = "Witaj " + name;
+        ViewData["NumTimes"] = numTimes;
+
+        return View();
     }
 }
--- Dotnet/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a41c898da6f7c0dd0c1cf9047f5b2a1ed72fb57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5bd71e12894daf6c73511d55dfa301b9fbae54")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
